0bf0de5fc6b335d660342f334608301e9c739a79
Michael Andersen <mian@kfst.dk>
2021-10-27 13:05:02 +0200

    Initial commit

---
+++ multi_directory_corpus_reader.py
@@ -0,0 +1,78 @@
+from glob import glob
+from itertools import chain, product
+from typing import Callable, List
+
+import os
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s',
+    level=logging.INFO)
+
+
+class MultiDirectoryCorpusReader:
+    def __init__(self,
+                 input_dirs: List[str],
+                 glob_filters: List[str],
+                 preprocessor_func: Callable[[str], List[str]]=None,
+                 print_progress: bool=False):
+        """MultiDictionaryCorpusReader
+
+        Usage: Streams **raw text** content from multiple directories using globbing for matching
+        file types.
+
+        Example 1, given the directory structure:
+
+        data
+        ├─ source1
+        |  ├─ file1.txt
+        |  ├─ file2.txt
+        |  └─ file3.msg
+        └─ source2
+           ├─ file1.msg
+           ├─ file2.doc
+           ├─ file3.txt
+           └─ file4.text
+
+        mdcr = MultiDirectoryCorpusReader(input_dirs=['data/source1', 'data/source2'], glob_filters=['*.txt', '*.msg', '*.doc', '*.text'])
+
+        The above provides an iterator that yields the text content of all files ending with txt,
+        msg, doc and text in the source1 and source2 directories. In addition it supports passing
+        a preprocess function which is applied to the raw text read from the files.
+
+        Example 2, passing a preprocess function and logging progress
+
+        from gensim.utils import simple_preprocess
+
+        mdcr = MultiDirectoryCorpusReader(
+            input_dirs=['data/source1', 'data/source2'],
+            glob_filters=['*.txt', '*.msg', '*.doc', '*.text'],
+            preprocess_func=simple_preprocess,
+            print_progress=True)
+
+
+        """
+        self.print_progress = print_progress
+        self.preprocessor_func = preprocessor_func
+        self._files = list(chain(*[glob(os.path.join(p, gf)) for p, gf in product(input_dirs, glob_filters)]))
+        logging.info(f'Found #{len(self.files)} files')
+
+    def __iter__(self):
+        for i, f in enumerate(self.files):
+            if self.print_progress and i > 0 and i % 10000 == 0:
+                logging.info(f"Read {i} files")
+            with open(f, 'r') as fd:
+                content = fd.read()
+                if content == '':
+                    continue
+                if self.preprocessor_func:
+                    yield self.preprocessor_func(content)
+                else:
+                    yield content
+
+    def __len__(self):
+        return len(self.files)
+
+    @property
+    def files(self):
+        return self._files
---
+++ test/test_multi_directory_corpus_reader.py
@@ -0,0 +1,47 @@
+from typing import List
+
+import os
+import pytest
+
+from multi_directory_corpus_reader import MultiDirectoryCorpusReader
+
+@pytest.fixture
+def default_sources_mdcr():
+    source1 = 'test/data/source1'
+    source2 = 'test/data/source2'
+
+    mdcr = MultiDirectoryCorpusReader(input_dirs=[source1, source2], glob_filters=['*.txt', '*.msg'])
+    yield source1, source2, mdcr
+
+def test_initialization(default_sources_mdcr):
+    source1, source2, mdcr = default_sources_mdcr
+
+    expected_result: List[str] = sorted([os.path.join(source1, p) for p in os.listdir(source1)] +
+                                        [os.path.join(source2, p) for p in os.listdir(source2)])
+
+    result = sorted(mdcr.files)
+
+    assert expected_result == result
+    assert len(expected_result) == len(result)
+
+def test_len_dunder(default_sources_mdcr):
+    source1, source2, mdcr = default_sources_mdcr
+
+    expected_result: int = len(sorted([os.path.join(source1, p) for p in os.listdir(source1)] +
+                                            [os.path.join(source2, p) for p in os.listdir(source2)]))
+
+    result: int = len(mdcr)
+
+    assert expected_result == result
+
+def test_preprocessor_func(default_sources_mdcr):
+    source1, source2, _ = default_sources_mdcr
+
+    def preprocessor_tokenize_remove_a(s: str) -> List[str]:
+        return s.replace('a', '').split(' ')
+
+    mdcr = MultiDirectoryCorpusReader(input_dirs=[source1, source2], glob_filters=['*.txt', '*.msg'], preprocessor_func=preprocessor_tokenize_remove_a)
+    # Test that all tokenized documents have a length larger than 0 and that none of the tokens contains 'a'
+    for res in mdcr:
+        assert len(res) > 0
+        assert all([not 'a' in token for token in res])
