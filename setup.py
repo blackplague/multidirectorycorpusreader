19ba17e7ac7270fc8248313b152a6adc68fa0f9a
Michael Andersen <mian@kfst.dk>
2021-11-01 09:40:42 +0100

    Update setup.py
      - Now uses pathlib for README.md file
      - Bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
+import pathlib
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
-    readme = f.read()
+FILE_LOCATION = pathlib.Path(__file__).parent
+README = (FILE_LOCATION / 'README.md').read_text()
 
 setup(
     name='MultiDirectoryCorpusReader',
-    version='0.1.0',
+    version='0.2.0',
     description='Easier multi directory source globbing of text files',
+    long_description=README,
+    long_description_content_type='text/markdown',
     author='Michael Andersen',
     author_email='gosuckadeadcow+github@gmail.com',
     url='https://github.com/blackplague/multidirectorycorpusreader',
